d46901ffbde5a62828f58f65c7c7da6da56c4a9e
jbielt <jbielturon@gmail.com>
2021-07-14 15:48:41 +0200

    refactor

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,6 +1,5 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { Todo } from 'src/app/interfaces/todo';
 import {TodoService} from '../../services/todo.service';
 
 @Component({
@@ -24,10 +23,8 @@ import {TodoService} from '../../services/todo.service';
 })
 export class TodoListComponent implements OnInit {
   todoTitle = '';
-  // private todoService: TodoService; //we do it at the constructor
 
   constructor(private todoService: TodoService) {
-    // this.todoService = todoService;
   }
 
   ngOnInit(): void {
@@ -42,60 +39,5 @@ export class TodoListComponent implements OnInit {
     this.todoService.addTodo(this.todoTitle);
     this.todoTitle = '';
   }
-  /*
-  editTodo(todo: Todo): void {
-    this.beforeEditCache = todo.title;
-    todo.editing = true;
-  }
-
-  doneEdit(todo: Todo): void {
-    if (todo.title.trim().length === 0){
-      todo.title = this.beforeEditCache;
-    }
-    todo.editing = false;
-  }
-
-  cancelEdit(todo: Todo): void {
-    todo.title = this.beforeEditCache;
-    todo.editing = false;
-
-  }
-
-  deleteTodo(id: number): void {
-    this.todos = this.todos.filter(todo => todo.id !== id);
-  }
-
-  remaining(): number {
-    return this.todos.filter(todo => !todo.completed).length;
-  }
-
-  atLeastOneCompleted(): boolean {
-    return this.todos.filter(todo => !todo.completed).length > 0;
-  }
-
-  clearCompleted(): void {
-    this.todos = this.todos.filter(todo => !todo.completed);
-  }
-
-
-  checkAllTodos(): void {
-    // @ts-ignore
-    const checkboxes = event.target as HTMLInputElement;
-    this.todos.forEach(todo => todo.completed = checkboxes.checked);
-  }
-
-  todosFiltered(): Todo[] {
-    if (this.filter === 'all'){
-      return this.todos;
-    }else if (this.filter === 'active'){
-      return this.todos.filter(todo => !todo.completed);
-    }else if (this.filter === 'completed'){
-      return this.todos.filter(todo => todo.completed);
-    }
-    return this.todos;
-  }
-
-   */
-
 }
 
